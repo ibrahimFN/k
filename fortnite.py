234aa3a9bdfbaa41cc9dfd76e598d33ea822b2df
OS20dusty28 <63958372+OS20dusty28@users.noreply.github.com>
2020-06-05 22:11:25 +0100

    Add files via upload

--- fortnite.py
+++ fortnite.py
@@ -578,6 +578,17 @@ async def event_friend_message(message):
 
         await message.reply(f'Skin set to shadow Peely')
         
+    if "!peelyghost" in args[0].lower():
+        await client.party.me.set_outfit(
+            asset='CID_701_Athena_Commando_M_BananaAgent',
+            variants=client.party.me.create_variants(
+                progressive=2
+                ),
+            enlightenment=(2, 350)
+        )
+
+        await message.reply(f'Skin set to ghost Peely')
+        
     if "!goldenmidas" in args[0].lower():
         await client.party.me.set_outfit(
             asset='CID_694_Athena_Commando_M_Catburglar',
